e0dae359664852196edc2bcb0b995b317e314536
jamil <jamilmaqdis@gmail.com>
2022-12-09 14:43:38 +0100

    Add Bind2, Bind3, MergeSources3

--- Tiny.Result/CE.fs
+++ Tiny.Result/CE.fs
@@ -13,6 +13,26 @@ module ResultCE =
     member inline _.Bind(result: Result<'a, 'e>, [<InlineIfLambda>] f: 'a -> Result<'b, 'e>) : Result<'b, 'e> =
       Result.bind f result
 
+    member inline _.Bind2
+      (
+        result1: Result<'a, 'e>,
+        result2: Result<'b, 'e>,
+        [<InlineIfLambda>] f: 'a * 'b -> Result<'c, 'e>
+      ) : Result<'c, 'e> =
+      result1 |> Result.bind (fun r1 -> result2 |> Result.bind (fun r2 -> f (r1, r2)))
+
+    member inline _.Bind3
+      (
+        result1: Result<'a, 'e>,
+        result2: Result<'b, 'e>,
+        result3: Result<'c, 'e>,
+        [<InlineIfLambda>] f: 'a * 'b * 'c -> Result<'d, 'e>
+      ) : Result<'d, 'e> =
+      result1
+      |> Result.bind (fun r1 ->
+        result2
+        |> Result.bind (fun r2 -> result3 |> Result.bind (fun r3 -> f (r1, r2, r3))))
+
     member inline _.Delay([<InlineIfLambda>] f: unit -> Result<'a, 'e>) : unit -> Result<'a, 'e> = f
 
     member inline _.Run([<InlineIfLambda>] f: unit -> Result<'a, 'e>) : Result<'a, 'e> = f ()
@@ -71,7 +91,15 @@ module ResultCE =
     member inline _.BindReturn(result: Result<'a, 'e>, [<InlineIfLambda>] f: 'a -> 'b) : Result<'b, 'e> =
       Result.map f result
 
-    member inline _.MergeSources(result1: Result<'a, 'e>, result2: Result<'b, 'e>) : Result<'a * 'b, 'e> =
-      result1 |> Result.bind (fun r1 -> result2 |> Result.map (fun r2 -> r1, r2))
+    member inline this.MergeSources(result1: Result<'a, 'e>, result2: Result<'b, 'e>) : Result<'a * 'b, 'e> =
+      this.Bind2(result1, result2, (fun (r1, r2) -> this.Return(r1, r2)))
+
+    member inline this.MergeSources3
+      (
+        result1: Result<'a, 'e>,
+        result2: Result<'b, 'e>,
+        result3: Result<'c, 'e>
+      ) : Result<'a * 'b * 'c, 'e> =
+      this.Bind3(result1, result2, result3, (fun (r1, r2, r3) -> this.Return(r1, r2, r3)))
 
   let result = ResultBuilder()
