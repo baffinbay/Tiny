267c4d6957ecaa89092df0e3499d63e1575945a8
jamil <jamilmaqdis@gmail.com>
2022-12-13 16:15:57 +0100

    Add andMap back and implement map2 and map3 using it

--- Tiny.Result/Result.fs
+++ Tiny.Result/Result.fs
@@ -5,16 +5,21 @@ module Result =
 
   let inline singleton (value: 'a) : Result<'a, 'e> = result { return value }
 
+  let inline apply (f: Result<'a -> 'b, 'e>) (result: Result<'a, 'e>) : Result<'b, 'e> =
+    ResultCE.result {
+      let! f = f
+      and! r = result
+      return f r
+    }
+
+  let inline andMap (result: Result<'a, 'e>) (f: Result<'a -> 'b, 'e>) : Result<'b, 'e> = apply f result
+
   let inline map2
     ([<InlineIfLambda>] f: 'a -> 'b -> 'c)
     (result1: Result<'a, 'e>)
     (result2: Result<'b, 'e>)
     : Result<'c, 'e> =
-    result {
-      let! r1 = result1
-      and! r2 = result2
-      return f r1 r2
-    }
+    singleton f |> andMap result1 |> andMap result2
 
   let inline map3
     ([<InlineIfLambda>] f: 'a -> 'b -> 'c -> 'd)
@@ -22,12 +27,7 @@ module Result =
     (result2: Result<'b, 'e>)
     (result3: Result<'c, 'e>)
     : Result<'d, 'e> =
-    result {
-      let! r1 = result1
-      and! r2 = result2
-      and! r3 = result3
-      return f r1 r2 r3
-    }
+    singleton f |> andMap result1 |> andMap result2 |> andMap result3
 
   let inline bimap
     ([<InlineIfLambda>] f: 'a -> 'b)
@@ -35,8 +35,6 @@ module Result =
     (result: Result<'a, 'e1>)
     : Result<'b, 'e2> =
     (Result.map f >> Result.mapError g) result
-
-  let inline apply (f: Result<'a -> 'b, 'e>) (result: Result<'a, 'e>) : Result<'b, 'e> = map2 (|>) result f
 
   let inline bindError ([<InlineIfLambda>] f: 'e1 -> Result<'a, 'e2>) (result: Result<'a, 'e1>) : Result<'a, 'e2> =
     match result with
